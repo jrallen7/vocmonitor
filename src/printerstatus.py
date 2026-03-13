7b1065e9d1e7ba54d2aba48ce596816999e75746
J.R. Allen <james@jamesrallen.xyz>
2026-03-13 08:54:34 -0500

    printerstatus formatting

--- src/printerstatus.py
+++ src/printerstatus.py
@@ -15,28 +15,35 @@ from pymemcache.client.base import Client
 sys.path.append(os.path.expanduser("~/bambu-connect"))
 from bambu_connect import BambuClient
 
+STATUSMSG_DUMP = False
 
-def bambu_status_callback(statusmsg):
-    status_data = {f: statusmsg.__getattribute__(f) for f in bambu_fields}
-    sharedcache.set_multi({k: v for k, v in status_data.items() if v is not None})
 
-    cache_data = sharedcache.get_multi(bambu_fields)
+def callback_status(statusmsg):
+    # parse statusmsg and push non-None values to cache
+    statusdata = {f: statusmsg.__getattribute__(f) for f in bambu_fields}
+    client_cache.set_multi({k: v for k, v in statusdata.items() if v is not None})
 
+    # get cache data
+    cachedata = client_cache.get_multi(bambu_fields)
+
+    timestamp = datetime.datetime.now().astimezone().isoformat(timespec="seconds")
     print(
         " ".join(
             [
-                datetime.datetime.now().astimezone().isoformat(timespec="seconds"),
-                "Nozzle: {nozzle_temper:.1f} ({nozzle_target_temper:.1f})",
+                timestamp,
+                "Hotend: {nozzle_temper:.1f} ({nozzle_target_temper:.1f})",
                 "Bed: {bed_temper:.1f} ({bed_target_temper:.1f})",
                 "Status: {mc_print_stage} {mc_percent}",
             ]
-        ).format(**cache_data)
+        ).format(**cachedata)
     )
-    # with open('msgdump.txt', 'at') as ftemp:
-    #    ftemp.write('{}\n\n'.format(statusmsg))
+    if STATUSMSG_DUMP:
+        with open("msgdump.txt", "at") as fdump:
+            fdump.write("{}".format(timestamp))
+            fdump.write("{}\n\n".format(statusmsg))
 
 
-def bambu_connect_callback():
+def callback_connect():
     print("Connecting to Printer")
 
 
@@ -45,34 +52,40 @@ if __name__ == "__main__":
     parser.add_argument("--flush", action="store_true")
     args = parser.parse_args()
 
-    with open(os.path.join(sys.path[0], "..", "config.toml"), "rb") as f:
-        configdata = tomllib.load(f)
+    with open(os.path.join(sys.path[0], "..", "config.toml"), "rb") as fconfig:
+        configdata = tomllib.load(fconfig)
 
     bambu_fields = [
-        "nozzle_target_temper",
-        "nozzle_temper",
-        "bed_target_temper",
-        "bed_temper",
-        "mc_print_stage",
-        "mc_percent",
+        "nozzle_temper",  # current hotend temp
+        "nozzle_target_temper",  # target hotend temp
+        "bed_temper",  # current bed temp
+        "bed_target_temper",  # target bed temp
+        "mc_print_stage",  # print status
+        "mc_percent",  # print percent complete
+        "layer_num",  # current layer
+        "total_layer_num",  # total layers in current print
     ]
-    sharedcache = Client(serde=serde.pickle_serde, **configdata["memcache"])
-    if args.flush:
-        sharedcache.flush_all()
-    _ = sharedcache.get_multi(bambu_fields)
-    for f in bambu_fields:
-        if f not in _:
-            shared.set(f, 0)
 
+    client_cache = Client(serde=serde.pickle_serde, **configdata["memcache"])
+
+    # initialize cache
+    if args.flush:
+        client_cache.flush_all()
+    temp = client_cache.get_multi(bambu_fields)
+    for f in bambu_fields:
+        if f not in temp:
+            client_cache.set(f, 0)
+
+    # main loop
     try:
         print("Starting Bambu Client")
-        bambu_client = BambuClient(**configdata["bambu"])
-        bambu_client.start_watch_client(bambu_status_callback, bambu_connect_callback)
+        client_bambu = BambuClient(**configdata["bambu"])
+        client_bambu.start_watch_client(callback_status, callback_connect)
 
         while True:
             sleep(0.1)
     except KeyboardInterrupt:
-        pass
+        print("break")
     finally:
         print("Closing Bambu Client")
-        bambu_client.stop_watch_client()
+        client_bambu.stop_watch_client()
